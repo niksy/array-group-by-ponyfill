8c6f0401aa9bfbe4ca920f9ad14c4749ff45b1ad
Ivan Nikolić <niksy5@gmail.com>
2021-11-11 11:00:51 +0100

    Add implementation

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-export default function () {}
+function ponyfill(array, callback) {
+	if (!Array.isArray(array)) {
+		throw new TypeError(`Can’t call method on ${array}`);
+	}
+
+	if (typeof callback !== 'function') {
+		throw new TypeError(`${callback} is not a function`);
+	}
+
+	const result = {};
+
+	array.forEach((value, index, array) => {
+		const key = callback.call(this, value, index, array);
+		result[key] ??= [];
+		result[key].push(value);
+	});
+
+	return result;
+}
+
+/* istanbul ignore next */
+function preferNative(array, callback) {
+	if (typeof Array.prototype.groupBy !== 'undefined') {
+		return array.groupBy(callback);
+	}
+	return ponyfill(array, callback);
+}
+
+export default ponyfill;
+
+export { preferNative };
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import function_ from '../index';
+import groupBy from '../index';
 
 before(function () {
 	window.fixture.load('/test/fixtures/index.html');
@@ -9,6 +9,46 @@ after(function () {
 	window.fixture.cleanup();
 });
 
-it('test!', function () {
-	// …
+it('handles simple array', function () {
+	const array = [1];
+	const context = {};
+	groupBy.call(context, array, function (...arguments_) {
+		const [value, key, that] = arguments_;
+		assert.equal(
+			arguments_.length,
+			3,
+			'Correct number of callback arguments'
+		);
+		assert.equal(value, 1, 'Correct value in callback');
+		assert.equal(key, 0, 'Correct index in callback');
+		assert.equal(that, array, 'Correct link to array in callback');
+		assert.equal(this, context, 'Correct callback context');
+	});
+});
+
+it('handles complex arrays', function () {
+	/* eslint-disable no-undefined */
+	assert.deepEqual(
+		groupBy([1, 2, 3], (value) => value % 2),
+		{ 1: [1, 3], 0: [2] },
+		'#1'
+	);
+	assert.deepEqual(
+		groupBy(
+			[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+			(value) => `i${value % 5}`
+		),
+		{ i1: [1, 6, 11], i2: [2, 7, 12], i3: [3, 8], i4: [4, 9], i0: [5, 10] },
+		'#2'
+	);
+	assert.deepEqual(
+		groupBy(Array.from({ length: 3 }), (value) => value),
+		{ undefined: [undefined, undefined, undefined] },
+		'#3'
+	);
+});
+
+it('handles invalid arguments', function () {
+	assert.throws(() => groupBy(null, () => {}), TypeError);
+	assert.throws(() => groupBy([], null), TypeError);
 });
